0074ecbed5185b81e44a47b9167b041a738782a5
Giheon Do <dgh0001@naver.com>
2025-09-09 15:08:24 +0900

    Initial commit

---
+++ src/main/java/knu/invigoworksknu/InvigoworksKnuApplication.java
@@ -0,0 +1,13 @@
+package knu.invigoworksknu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvigoworksKnuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvigoworksKnuApplication.class, args);
+    }
+
+}
